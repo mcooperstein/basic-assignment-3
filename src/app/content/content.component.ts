90f7e39a4eb508cf167e868a25b69303a2b67a9b
Marc Cooperstein <mcooperstein@gmail.com>
2018-02-07 15:21:54 -0800

    added instructor solution

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -3,31 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styles:[`
+  /*styles:[`
     .white-text{
         color: white;
     }
-  `],
-  //styleUrls: ['./content.component.css']
+  `],*/
+  styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
 
-    showParagraph = false;
+    showSecret = false;
+    log = [];
+
+    onToggleDetails(){
+        this.showSecret = !this.showSecret;
+        //var date = new Date();
+        this.log.push(this.log.length+1);
+        //this.log.push(date);
+    }
+
+    //my solution
+    /*showParagraph = false;
     buttonClicks = [];
+
+    onToggleParagraph(){
+    this.showParagraph= this.showParagraph===true?false:true;
+    this.buttonClicks.push("button click # " + (this.buttonClicks.length+1))
+    }*/
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onToggleParagraph(){
-    /*if(this.showParagraph==true){
-        this.showParagraph=false;
-    } else {
-        this.showParagraph=true;
-    }*/
-    this.showParagraph= this.showParagraph===true?false:true;
-    this.buttonClicks.push("button click # " + (this.buttonClicks.length+1))
-  }
+
 
 }
